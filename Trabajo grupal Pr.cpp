4eceda89f558100e5d247d29d8b9d3d09004375b
JaredBG24 <85966004+JaredBG24@users.noreply.github.com>
2021-06-21 23:04:50 -0500

    Update Trabajo grupal Pr.cpp
    mejore el algoritmo de la suma, corrigiendo errores que habían y simplificando el algoritmo

--- Trabajo grupal Pr.cpp
+++ Trabajo grupal Pr.cpp
@@ -49,11 +49,20 @@ for(int i=0;i<m;i++){
 	}
 }
 //suma de matrices Jared Bernal
-   for(int i=0;i<m;i++){
-	for(int j=0;j<n;j++){
-		cout<<mat1[i][j]+mat2[i][j];
-	}
-	cout<<endl;	
-}
+#include<iostream>
 
-    return 0;
+using namespace std;
+
+int main(){
+	int c=0, i=0;
+float matriz1[10]={2.3,4.5,6.5,0.5,6.1,5.5,1.2,2.4,6.5,9.1};
+float matriz2[10]={2.3,4.5,6.5,0.5,6.1,5.5,1.2,2.4,6.5,9.1};
+float suma[10],resta[10],producto[10],division[10];
+	
+	cout<<"La suma de las dos matrices es la siguiente: "<<endl;
+	for(i=0;i<10;i++){
+		suma[i] = matriz1[i] + matriz2[i];
+		cout << suma[i]<<endl;
+	}
+	return 0;
+}
