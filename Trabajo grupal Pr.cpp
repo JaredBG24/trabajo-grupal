3f0c7b2f918586cc2353b7d983ab54f0a02a6b75
L3onardoxd <86301298+L3onardoxd@users.noreply.github.com>
2021-06-22 15:24:11 -0500

    Update Trabajo grupal Pr.cpp

--- Trabajo grupal Pr.cpp
+++ Trabajo grupal Pr.cpp
@@ -93,7 +93,7 @@ float suma[10],resta[10],producto[10],division[10];
 using namespace std;
 	
 int main(){
-	int c=0, i=0;
+int i=0;
 float matriz1[10]={2.3,4.5,6.5,0.5,6.1,5.5,1.2,2.4,6.5,9.1};
 float matriz2[10]={2.3,4.5,6.5,0.5,6.1,5.5,1.2,2.4,6.5,9.1};
 float suma[10],resta[10],producto[10],division[10];
