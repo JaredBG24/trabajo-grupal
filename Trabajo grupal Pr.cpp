08d997d465122bd1be6f2c021da03494bf62cbef
Nahomi Alcivar <85972823+NahomiAlcivar14@users.noreply.github.com>
2021-06-21 20:13:52 -0500

    Update Trabajo grupal Pr.cpp

--- Trabajo grupal Pr.cpp
+++ Trabajo grupal Pr.cpp
@@ -1,18 +1,31 @@
 #include<iostream>
 using namespace std;
-int main()
-{
-int c=0;
-float matriz1[10]={2.3,4.5,6.5,0.5,6.1,5.5,1.2,2.4,6.5,9.1};
-float matriz2[10]={2.3,4.5,6.5,0.5,6.1,5.5,1.2,2.4,6.5,9.1};
-float suma[10],resta[10],producto[10],division[10];
-//suma de matrices Jared Bernal
-  for(int i=0;i<m;i++){
-	 for(int j=0;j<n;j++){
-		cout<<mat1[i][j]+mat2[i][j];
-	}
-	cout<<endl;	
-
-return 0;
-
-}
+int main(){
+    int m,n; //m=fila n=columnas
+    cout<<"SUMAR MATRICES"<<endl;
+    cout<<"Ingrese numero de filas"<<endl;
+    cin>>m;
+    cout<<"Ingrese numero de columnas"<<endl;
+    cin>>n;
+    
+    int mat1[m][n] , mat2[m][n];
+    cout<<"LLENADO DE LA MATRIZ 1:"<<endl;
+    for(int i=0;i<m;i++){
+    	for(int j=0;j<n;j++){
+    		cout<<"Ingrese numero:";
+    		cin>>mat1[i][j];
+    	}
+    }
+    cout<<"\n";
+    cout<<"*****************"<<endl;
+    cout<<"LLENADO DE MATRIZ 2"<<endl;
+    for(int i=0;i<m;i++){
+    	for(int j=0;j<n;j++){
+    		cout<<"Ingrese numero:";
+    		cin>>mat2[i][j];
+    	}
+    }
+    
+    //mostrar matriz
+    
+    return 0;
