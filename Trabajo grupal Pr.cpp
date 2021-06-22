db9ab7695ac1538285ea616298fb6855936601d9
JaredBG24 <85966004+JaredBG24@users.noreply.github.com>
2021-06-22 16:10:57 -0500

    Update Trabajo grupal Pr.cpp

--- Trabajo grupal Pr.cpp
+++ Trabajo grupal Pr.cpp
@@ -1,53 +1,21 @@
-operacionesbasicas
 #include<iostream>
 using namespace std;
-int main(){
-    int m,n; //m=fila n=columnas
-    cout<<"SUMAR MATRICES"<<endl;
-    cout<<"Ingrese numero de filas"<<endl;
-    cin>>m;
-    cout<<"Ingrese numero de columnas"<<endl;
-    cin>>n;
-    
-    int mat1[m][n] , mat2[m][n];
-    cout<<"LLENADO DE LA MATRIZ 1:"<<endl;
-    for(int i=0;i<m;i++){
-    	for(int j=0;j<n;j++){
-    		cout<<"Ingrese numero:";
-    		cin>>mat1[i][j];
-    	}
-    }
-    cout<<"\n";
-    cout<<"*****************"<<endl;
-    cout<<"LLENADO DE MATRIZ 2"<<endl;
-    for(int i=0;i<m;i++){
-    	for(int j=0;j<n;j++){
-    		cout<<"Ingrese numero:";
-    		cin>>mat2[i][j];
-    	}
-    }
-    
-    //mostrar matriz nahomi alcivar
-    
-    return 0;
-	
-cout<<"\n";
-cout<<"La matriz 1 es:"<<endl;
-for(int i=0;i<m;i++){
-	for(int j=0;j<n;j++){
-		cout<<"Ingrese numero:";
-		cin>>mat1[i][j];
-	}
-}
+int main()
+{
+int i=0;
+float matriz1[10]={2.3,4.5,6.5,0.5,6.1,5.5,1.2,2.4,6.5,9.1};
+float matriz2[10]={2.3,4.5,6.5,0.5,6.1,5.5,1.2,2.4,6.5,9.1};
+float suma[10],resta[10],producto[10],division[10];
 
-cout<<"\n";
-cout<<"La matriz 2 es:"<<endl;
-for(int i=0;i<m;i++){
-	for(int j=0;j<n;j++){
-		cout<<"Ingrese numero:";
-		cin>>mat2[i][j];
+//Nahomi Alcivar
+
+for(int i=0;i<10;i++){
+		cout<<matriz1[i]<<" ";
 	}
-}
+	cout<<endl;
+	for(int i=0;i<10;i++){
+		cout<<matriz2[i]<<" ";
+	}
 //suma de matrices-Jared Bernal
 #include<iostream>
 
